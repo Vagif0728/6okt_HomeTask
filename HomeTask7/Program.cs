5a65d658d28d6544571c8ef82fc7c881d41a4d2e
Vagif0728 <tu7j4ryqj@code.edu.az>
2023-10-08 13:53:59 +0400

    new task

---
+++ HomeTask7/Program.cs
@@ -0,0 +1,122 @@
+﻿//namespace HomeTask7
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+            //-----------------------------------Task 2 ----------------------------------------- 
+
+          //  Console.WriteLine("Xallari daxil edin : ");
+          //  int [] rewad = new int[3];
+          //  for (int i = 0; i <rewad.Length; i++)
+          //  {
+          //      rewad[i]=int.Parse(Console.ReadLine());
+          //  }
+
+          //  for (int i = 0; i <rewad.Length; i++)
+          //  {
+          //      Console.WriteLine(rewad[i]);
+          //  }
+
+
+          //  Console.WriteLine();
+          //  Console.WriteLine();
+
+          //  Console.WriteLine("Xallari daxil edin : ");
+          //  int[] aydan = new int[3];
+          //  for (int i = 0; i < aydan.Length; i++)
+          //  {
+          //      aydan[i] = int.Parse(Console.ReadLine());
+          //  }
+
+          //  for (int i = 0; i < aydan.Length; i++)
+          //  {
+          //      Console.WriteLine(aydan[i]);
+          //  }
+
+
+          //  Console.WriteLine();
+          //  Console.WriteLine();
+
+          //  int aydanPoint = 0;
+          //  int rewadPoint = 0;
+
+          //Football(rewad, aydan, ref rewadPoint, ref  aydanPoint);
+          //  Basketball(rewad, aydan, ref rewadPoint, ref aydanPoint);
+          //  Tennis(rewad, aydan, ref rewadPoint, ref aydanPoint);
+
+
+
+//---------------------------------------------------Task 3 ---------------------------------------------
+
+            //string word=Console.ReadLine();
+
+            //Words(word);
+
+        //}
+
+
+
+//--------------------------------------------Task 2 -----------------------------------------------
+
+
+        //public static void Football(int[]rewad,int[]aydan,ref int rewadPoint,ref int aydanPoint)
+        //{
+        //    if (rewad[0] > aydan[0]) 
+        //    {
+        //        Console.WriteLine(++rewadPoint);
+        //    }
+        //   else if (aydan[0] > rewad[0]) 
+        //    {
+        //        Console.WriteLine(++aydanPoint);
+        //    }
+
+           
+
+           
+        //}
+
+        //public static void Basketball(int[] rewad, int[] aydan, ref int rewadPoint, ref int aydanPoint)
+        //{
+        //     if (rewad[1] > aydan[1])
+        //    {
+        //        Console.WriteLine(++rewadPoint);
+        //    }
+        //     if (aydan[1] > rewad[1])
+        //    {
+        //        Console.WriteLine(++aydanPoint);
+        //    }
+        //}
+
+
+        //public static void Tennis(int[] rewad, int[] aydan, ref int rewadPoint, ref int aydanPoint)
+        //{
+        //    if (rewad[2] > aydan[2])
+        //    {
+        //        Console.WriteLine(++rewadPoint);
+        //    }
+        //     if (aydan[2] > rewad[2])
+        //    {
+        //        Console.WriteLine(++aydanPoint);
+        //    }
+        //}
+
+//----------------------------------------------------Task 3-------------------------------------------
+
+        //public static void Words(string word)
+        //{
+        //    for (int i = 0; i <word.Length; i++)
+        //    {
+        //        if (word[i] == ' ')
+        //        {
+        //            int j = i;
+        //            for ( j = 0; j < word.Length-1; j++)
+        //            {
+        //                Console.WriteLine(word[j]);
+        //            }
+        //        }
+        //    }
+
+        //}
+//    }
+//}
